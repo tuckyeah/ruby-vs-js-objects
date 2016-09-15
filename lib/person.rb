419c82cbb2d1047a2801a7e1213d832a61f4b061
tuckyeah <h.rosebrock@gmail.com>
2016-09-15 11:59:16 -0400

    add literal getters/setters in person.rb from lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,46 @@
 require 'pry'
+
+class Person
+  def initialize(name, surname, fav_food, catchphrase)
+    @given_name = name
+    @surname = surname
+    @favorite_food = fav_food
+    @catchphrase = catchphrase
+  end
+
+  # getters
+
+  def given_name
+    @given_name
+  end
+
+  def surname
+    @surname
+  end
+
+  def favorite_food
+    @favorite_food
+  end
+
+  def catchphrase
+    @catchphrase
+  end
+
+  # setters
+
+  def given_name=(new_name)
+    @given_name = new_name
+  end
+
+  def surname=(new_surname)
+    @surname = new_surname
+  end
+
+  def favorite_food=(new_food)
+    @favorite_food = new_food
+  end
+
+  def catchphrase=(phrase)
+    @catchphrase = phrase
+  end
+end
